35b905bd3500262e8dc7fd58077371e7e5ad2daf
manuels12345 <92234244+manuels12345@users.noreply.github.com>
2022-09-30 19:59:21 -0300

    agregado de componente libro

--- proyectoBibliotecaAngular/src/app/app-routing.module.ts
+++ proyectoBibliotecaAngular/src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { PrincipalComponent } from './pages/principal/principal.component';
 import { PrestamosComponent } from './pages/prestamos/prestamos.component';
 import { DevolucionesComponent } from './pages/devoluciones/devoluciones.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { LibrosComponent } from './pages/libros/libros.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'inicio'},
   {path: 'principal', component:PrincipalComponent },
   {path:'prestamos', component:PrestamosComponent},
   {path: 'devoluciones',component:DevolucionesComponent},
-  {path:'usuarios', component:UsuariosComponent}
+  {path:'usuarios', component:UsuariosComponent},
+  {path:'libros', component:LibrosComponent}
 
 
 ];
---
+++ proyectoBibliotecaAngular/src/app/pages/libros/libros.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Libro } from 'src/app/models/Libro';
+import { LibrosService } from 'src/app/servicios/libro.service';
+
+
+@Component({
+  selector: 'app-libros',
+  templateUrl: './libros.component.html',
+  styleUrls: ['./libros.component.css']
+})
+export class LibrosComponent implements OnInit {
+
+  public libros: Libro[] = [];
+
+  constructor(private librosService: LibrosService) { }
+
+  ngOnInit(): void {
+    this.cargarLibros();
+  }
+
+  cargarLibros() {
+    return this.librosService.cargarLibros().subscribe((data) => {
+      data.forEach((dato) =>{
+        this.libros.push(dato);
+      });
+      console.log(data);
+      return data;
+    });
+  }
+
+}
