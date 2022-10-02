e8c8736c2f2f7d179dd6602f803b09e6fa183f15
Juan López <juanem@hotmail.com>
2022-10-02 20:24:51 -0300

    b

--- proyectoBibliotecaAngular/src/app/pages/libros/libros.component.ts
+++ proyectoBibliotecaAngular/src/app/pages/libros/libros.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Libro } from 'src/app/models/Libro';
 import { LibrosService } from 'src/app/servicios/libro.service';
@@ -12,9 +13,10 @@ import { LibrosService } from 'src/app/servicios/libro.service';
 
 export class LibrosComponent implements OnInit {
 
+  idLibro!:number;
   libros: Libro[] = [];
 
-  constructor(private librosService: LibrosService) { }
+  constructor(private librosService: LibrosService, private router: Router) { }
 
   ngOnInit(): void {
     this.cargarLibros();
