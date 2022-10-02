b6c1c967275f5c2a505071934e2e15d2b18fb062
dakoo096 <noelia_cabral96@hotmail.com>
2022-10-02 18:02:54 -0300

    se realiza tabla de componente usuario,se realiza nuevo componente nuevo usuario,se realiza routing a nuevo componente

---
+++ proyectoBibliotecaAngular/src/app/pages/nuevo-usuario/nuevo-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-usuario',
+  templateUrl: './nuevo-usuario.component.html',
+  styleUrls: ['./nuevo-usuario.component.css']
+})
+export class NuevoUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
