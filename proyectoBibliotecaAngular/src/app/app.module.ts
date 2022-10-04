bfa88472a523e0dab328f0d2de0c9b39b1b1e37e
manuels12345 <92234244+manuels12345@users.noreply.github.com>
2022-10-03 23:18:48 -0300

    Se le agrega banner a las paginas

--- proyectoBibliotecaAngular/src/app/app.module.ts
+++ proyectoBibliotecaAngular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { LibrosComponent } from './pages/libros/libros.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NuevoUsuarioComponent } from './pages/nuevo-usuario/nuevo-usuario.component';
+import { BannerComponent } from './layouts/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { NuevoUsuarioComponent } from './pages/nuevo-usuario/nuevo-usuario.compo
     LoginComponent,
     NuevoUsuarioComponent,
     LibrosComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
